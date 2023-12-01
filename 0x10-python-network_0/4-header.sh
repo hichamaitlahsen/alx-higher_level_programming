578f08e89e64985f28f98eda77b4b81833245110
hichamaitlahsen5 <hichamaitlahsen5@gmail.com>
2023-11-30 16:00:34 -0800

    update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes an argument, sends a GET request displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+curl -sX GET $1 -H "X-School-User-Id: 98" -L
