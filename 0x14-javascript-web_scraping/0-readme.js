018234962977b8e9180dac87e61ed68b7100949d
hichamaitlahsen5 <hichamaitlahsen5@gmail.com>
2024-01-04 07:53:52 -0800

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fi_s = require('fi_s');
+const fs = require('fs');
 
-fi_s.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
